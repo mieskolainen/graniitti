2f525aa15f75bd275bd2adfad3d574288af2fcba
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2019-03-09 20:43:05 +0200

    cleaning updates

--- obj/HelAmps_sm_gg_ggg.d
+++
@@ -1,4 +0,0 @@
-obj/HelAmps_sm_gg_ggg.o: src/Amplitude/HelAmps_sm_gg_ggg.cc \
- include/Graniitti/Amplitude/HelAmps_sm_gg_ggg.h
-
-include/Graniitti/Amplitude/HelAmps_sm_gg_ggg.h:
--- obj/HelAmps_sm_yy_ll_2to4.d
+++
@@ -1,4 +0,0 @@
-obj/HelAmps_sm_yy_ll_2to4.o: src/Amplitude/HelAmps_sm_yy_ll_2to4.cc \
- include/Graniitti/Amplitude/HelAmps_sm_yy_ll_2to4.h
-
-include/Graniitti/Amplitude/HelAmps_sm_yy_ll_2to4.h:
