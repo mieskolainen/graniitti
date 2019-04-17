bfd35513ddec8bdf6b65c32041cad5a5e14756f3
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2019-04-17 04:44:48 +0300

    add flatamp

--- src/MProcess.cc
+++ src/MProcess.cc
@@ -470,8 +470,9 @@ double MProcess::GetFlatAmp2(const gra::LORENTZSCALAR& lts) const {
 	}
 	// Ansatz: |A|^2 ~ exp(bt1) exp(bt2) x exp(-shat)
 	else if (FLATAMP == 4) {
+		const double a = 1.75;
 		W = std::exp(PARAM_FLAT::b * lts.t1) *
-		    std::exp(PARAM_FLAT::b * lts.t2) * std::exp(-1.5*lts.s_hat);
+		    std::exp(PARAM_FLAT::b * lts.t2) * std::exp(-a*lts.s_hat);
 	}
 	// Constant
 	else if (FLATAMP == 5) {
@@ -479,7 +480,7 @@ double MProcess::GetFlatAmp2(const gra::LORENTZSCALAR& lts) const {
 	} else {
 		// Throw an error, unknown mode
 		std::string str =
-		    "MProcess::GetFlatAmp2: Unknown FLATAMP mode (|A|^2 is 0 = off, 1 = exp{b(t1+t2)}, 2 = exp{b(t1+t2)}/shat^{1/2}, 3 = exp{b(t1+t2)}/shat, 4 = exp{b(t1+t2)} x exp(-1.5*shat), 5 = 1.0) : input was " + std::to_string(FLATAMP);
+		    "MProcess::GetFlatAmp2: Unknown FLATAMP mode (|A|^2 is 0 = off, 1 = exp{b(t1+t2)}, 2 = exp{b(t1+t2)}/shat^{1/2}, 3 = exp{b(t1+t2)}/shat, 4 = exp{b(t1+t2)} x exp(-a*shat), 5 = 1.0) : input was " + std::to_string(FLATAMP);
 		throw std::invalid_argument(str);
 	}
 	return W;
