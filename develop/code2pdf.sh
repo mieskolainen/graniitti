bc8c12f0261bfcac615f3c82d6fa3cce5e3094b0
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2019-04-24 00:28:03 +0300

    update scripts

--- develop/code2pdf.sh
+++ develop/code2pdf.sh
@@ -4,3 +4,6 @@
 
 find ./src/ -name "M*.cc" | xargs enscript --color=1 -C -Ecpp -fCourier8 -o - | ps2pdf - code_cc.pdf
 find ./include/ -name "M*.h" | xargs enscript --color=1 -C -Ecpp -fCourier8 -o - | ps2pdf - code_h.pdf
+
+# combine
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dAutoRotatePages=/None -sOutputFile=merged.pdf code_h.pdf code_cc.pdf
