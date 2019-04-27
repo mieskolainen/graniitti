1c5c39f29622d673f54cda40b1e2e872c438ba99
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2019-04-27 11:35:42 +0300

    update installation

--- install/setenv.sh
+++ install/setenv.sh
@@ -10,13 +10,17 @@
 INSTALLPATH=$HOME/local
 # ***
 
+# lib64 needed on some systems
 export HEPMC3SYS=${INSTALLPATH}/HEPMC3
 export PATH=${PATH}:${HEPMC3SYS}/bin
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HEPMC3SYS}/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HEPMC3SYS}/lib64
 
+# lib64 needed on some systems
 export LHAPDFSYS=${INSTALLPATH}/LHAPDF
 export PATH=${PATH}:${LHAPDFSYS}/bin
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LHAPDFSYS}/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LHAPDFSYS}/lib64
 
 echo 'New environment variables:'
 echo ''
