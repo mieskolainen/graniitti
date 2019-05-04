3ad9c9fc17b395c591ce74705585a80881b2958a
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2019-05-04 15:58:59 +0300

    build automation

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# GRANIITTI [beta dev-branch / see VERSION.json] 
+# GRANIITTI [beta dev-branch / see VERSION.json]
 https://arxiv.org/abs/1811.01730
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 
 ## Algorithmic Engine and Monte Carlo Event Generator for High Energy Diffraction
+
+![alt text](https://raw.githubusercontent.com/mieskolainen/GRANIITTI/master/install/build-status.svg)
 
 
 ### Installation and introduction at:
---
+++ install/nightlybuild.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+echo '* GRANIITTI automated nightly build script *'
+
+# ------------------------------------------------
+# Set the manually here
+NAME="GRANIITTI"
+VERSION="0.35"
+TYPE="beta"
+DATE="28/04/2019"
+UPDATE="Algorithm updates"
+
+BUILD_DATE=`date`
+echo $BUILD_DATE
+# ------------------------------------------------
+
+# Fetch latest version, try to compile
+#git pull origin master && source ./install/setenv.sh && make superclean && make -j4
+
+# Test if compilation went fine
+FILE=./bin/gr
+
+if test -f "$FILE"; then
+  echo "Build successful"
+  BUILD_SUCCESS="true"
+  cp ./install/passing.svg ./install/build-status.svg
+else
+  echo "Build not successful"
+  BUILD_SUCCESS="false"
+  cp ./install/failing.svg ./install/build-status.svg
+fi
+
+# Output message
+MSG="{\n  name: \"$NAME\",\n version: \"$VERSION\",\n type: \"$TYPE\",\n date: \"$DATE\",\n update: \"$UPDATE\",\n build_success: $BUILD_SUCCESS,\n build_date: \"$BUILD_DATE\"\n}"
+
+echo -e $MSG > VERSION.json
+
