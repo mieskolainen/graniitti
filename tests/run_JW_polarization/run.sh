26e75e106f707868a3d74b1816c70c0d76b5d0db
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2019-10-21 06:15:20 +0300

    nightly build

--- tests/run_JW_polarization/run.sh
+++ tests/run_JW_polarization/run.sh
@@ -52,7 +52,7 @@ S2_photo=0.7
 -u ub \
 -R false \
 -t '#sqrt{s} = 13 TeV, |#eta| < 2.5, p_{T} > 0.15 GeV' \
--S "$S2, $S2_photo, $S2_photo, $S2, $S2, $S2" #-X 1000
+-S "$S2, $S2_photo, $S2_photo, $S2, $S2, $S2" -X 1000
 
 
 #convert -density 600 -trim .h1_S_M_logy.pdf -quality 100 output.jpg
