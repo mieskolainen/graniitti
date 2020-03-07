7f5b464eeeeb70c1561b6bfbc021c3deb7a35511
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2020-03-07 21:51:49 +0000

    clean script

--- develop/code2pdf.sh
+++ develop/code2pdf.sh
@@ -5,19 +5,19 @@
 # makefile
 COMMAND="--verbose --color=1 -C -fCourier8 -o -"
 
-find Makefile | xargs enscript -Emakefile --header='$n|$%/$=|' $COMMAND | ps2pdf - code_makefile.pdf
+find Makefile | xargs enscript -Emakefile --header='$N|$%/$=|' $COMMAND | ps2pdf - code_makefile.pdf
 
 # MadGraph to Graniitti conversion
-find ./develop/MG2GRA/MG2GRA.py | xargs enscript -Epython --header='$n|$%/$=|' $COMMAND | ps2pdf - code_MG2GRA.pdf
+find ./develop/MG2GRA/MG2GRA.py | xargs enscript -Epython --header='$N|$%/$=|' $COMMAND | ps2pdf - code_MG2GRA.pdf
 
 # -----------------------------
 
 # cc files (one can combine output of commands with (command1; command2; ... commandN)
 (find ./src/ -name "M*.cc" & \
- echo ./src/Program/gr.cc) | xargs enscript -Ecpp --header='$n|$%/$=|' $COMMAND | ps2pdf - code_cc.pdf
+ echo ./src/Program/gr.cc) | xargs enscript -Ecpp --header='$N|$%/$=|' $COMMAND | ps2pdf - code_cc.pdf
 
 # h files
-find ./include/ -name "M*.h" | xargs enscript -Ecpp --header='$n|$%/$=|' $COMMAND | ps2pdf - code_h.pdf
+find ./include/ -name "M*.h" | xargs enscript -Ecpp --header='$N|$%/$=|' $COMMAND | ps2pdf - code_h.pdf
 
 # combine
 #gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dAutoRotatePages=/None -sOutputFile=merged.pdf code_h.pdf code_cc.pdf
