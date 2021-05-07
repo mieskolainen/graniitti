055039e47166c9b54ab90ba11119cce966f2edb0
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2021-05-08 00:16:02 +0300

    update install

--- install/installGCC9.sh
+++ install/installGCC9.sh
@@ -1,3 +1,16 @@
 # See: https://anaconda.org/conda-forge/gcc_linux-64
+#
+# Show all: ls $CONDA_PREFIX/bin
 
 conda install -c conda-forge gcc_linux-64 
+conda install -c conda-forge gxx_linux-64 
+
+# Set compilers before compiling anything
+export CC=$CONDA_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc
+export CXX=$CONDA_PREFIX/bin/x86_64-conda_cos6-linux-gnu-g++
+
+# Compile HepMC3 and LHAPDF
+# cd install && source autoinstall.sh && cd ..
+
+# Compile
+# make -j8 TEST=TRUE CXX=$CXX CXX_STANDARD=c++17
