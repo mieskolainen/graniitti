761edfa375a1e2ee0b3123bd2062ef3b8fb79f03
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2021-05-09 03:03:15 +0300

    optimize

--- include/Graniitti/MMath.h
+++ include/Graniitti/MMath.h
@@ -198,7 +198,15 @@ inline bool CheckEMC(const M4Vec &diff, double epsilon = 1e-6) {
 }
 
 // Factorial n! = n*(n-1)*(n-2)*...*1
-constexpr double factorial(int n) {
+inline double factorial(int n) {
+  
+  static const double f[] = {1., 1., 2., 6., 24., 120., 720., 5040., 40320., 362880., 3628800., 39916800.,
+    479001600., 6227020800., 87178291200., 1307674368000., 20922789888000., 355687428096000., 6402373705728000.,
+    121645100408832000., 2432902008176640000., 51090942171709440000., 1124000727777607680000.};
+  if (n <= 23) {
+    return f[n];
+  }
+  
   double x = 1;
   for (int i = 1; i <= n; ++i) { x *= i; }
   return x;
